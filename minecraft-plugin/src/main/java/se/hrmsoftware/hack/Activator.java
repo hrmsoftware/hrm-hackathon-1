374fa11bef4f5f5e0b6b12371ca45669890dee0d
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-04-28 14:30:02 +0200

    Create sign on command

--- minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
+++ minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
@@ -1,11 +1,17 @@
 package se.hrmsoftware.hack;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class Activator extends JavaPlugin implements Listener {
 
@@ -26,6 +32,22 @@ public class Activator extends JavaPlugin implements Listener {
 
 	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		System.out.println("Command issued by "+sender+": " + command);
+		Player pl = getServer().getPlayer(sender.getName());
+		if (pl != null) {
+			Location loc = pl.getLocation();
+			loc.setX(loc.getX() + 5);
+			Block block = loc.getBlock();
+
+			block.setType(Material.SIGN_POST);
+
+			Sign sign = (Sign) block.getState();
+			sign.setLine(0, "Foo");
+			sign.setLine(1, "Fuck");
+			sign.update();
+
+			sender.sendMessage("DONE!");
+			return true;
+		}
 		return false;
 	}
 }
