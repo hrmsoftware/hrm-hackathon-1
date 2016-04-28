ba9c4b76b90f07cff863050768ad822f2c8aeb88
Henrik <henrik.grankvist@hrmsoftware.se>
2016-04-28 17:47:28 +0200

    Tree index is possible at a given location. Tweaking tweaking we need.

--- minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
+++ minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
@@ -34,20 +34,64 @@ public class Activator extends JavaPlugin implements Listener {
 		System.out.println("Command issued by "+sender+": " + command);
 		Player pl = getServer().getPlayer(sender.getName());
 		if (pl != null) {
-			Location loc = pl.getLocation();
-			loc.setX(loc.getX() + 5);
-			Block block = loc.getBlock();
+			double treeIndex = calculateTreeIndex(pl.getLocation(), 10);
 
+			Location loc = pl.getLocation();
+			loc.add(loc.getDirection().setY(0).normalize().multiply(1));
+
+			Block block = loc.getBlock();
 			block.setType(Material.SIGN_POST);
 
 			Sign sign = (Sign) block.getState();
-			sign.setLine(0, "Foo");
-			sign.setLine(1, "Fuck");
+			sign.setLine(0, "Tree Index");
+			sign.setLine(1, Double.toString(treeIndex));
 			sign.update();
 
-			sender.sendMessage("DONE!");
+			sender.sendMessage("DONE! Tree Index:" + treeIndex);
+
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * The tree index is calculated from all the blocks with the type LEAF within a cube with the side blocks*2.
+	 *
+	 * The index is calculated by counting all the leaf blocks and multiply this by 3 and then divide this with the
+	 * total number of blocks in the cube.
+	 *
+	 * @param loc the location where we need to calculate the index.
+	 * @param blocks the number of blocks, times two that will be the side of the box. This means that the measure
+	 *               point is almost in the center bottom of the cube.
+	 *
+	 * @return the index.
+	 */
+	private double calculateTreeIndex(Location loc, int blocks) {
+		int scanBoxSide = blocks * 2;
+
+		double initalX = loc.getX() + blocks;
+		double initalY = loc.getY() + scanBoxSide;
+		double initalZ = loc.getZ() + blocks;
+
+		int count = 0;
+
+		for (int x = 0; x < scanBoxSide; x++) {
+			loc.setX(initalX - x);
+			for (int y = 0; y <= scanBoxSide; y++) {
+				loc.setY(initalY - y);
+				for(int z = 0; z < scanBoxSide; z++) {
+					loc.setZ(initalZ - z);
+					Block b = loc.getBlock();
+
+					if (b.getType().equals(Material.LEAVES) || b.getType().equals(Material.LEAVES_2)) {
+						count++;
+					}
+
+					//b.setType(Material.GLASS);
+				}
+			}
+		}
+
+		return (double) count * 10 / ((double)scanBoxSide * scanBoxSide * scanBoxSide);
 	}
 }
