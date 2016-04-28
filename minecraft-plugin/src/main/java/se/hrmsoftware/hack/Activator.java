955cb6508fd14103e92ea8e00f06ef76b7a0d47c
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-04-28 13:49:37 +0200

    Add simple event listener

--- minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
+++ minecraft-plugin/src/main/java/se/hrmsoftware/hack/Activator.java
@@ -1,14 +1,24 @@
 package se.hrmsoftware.hack;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Activator extends JavaPlugin {
+public class Activator extends JavaPlugin implements Listener {
 
 	@Override public void onEnable() {
 		getLogger().info("ENABLED");
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@Override public void onDisable() {
 		getLogger().info("DISABLED");
+	}
+
+
+	@EventHandler
+	public void handle(PlayerLoginEvent evt) {
+		getLogger().info("Player logged in: " + evt.getPlayer().getDisplayName());
 	}
 }
